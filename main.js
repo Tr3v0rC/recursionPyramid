b21fd810f5a4d60733bb3218b10d1ce0218b6b9d
Tr3v0r_C <trevor.clarke62@gmail.com>
2017-10-15 13:01:03 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+window.onload = setup;
+var width, height,;
+
+function setup() {
+    canvas = document.getElementById("c");
+    c = canvas.getContext("2d");
+    sizeCanvas();
+    draw(width / 2, 100, 100);
+}
+
+function draw(x, y, w) {
+    line(x, y, x - w, y + w);
+    line(x, y, x + w, y + w);
+    console.log(w);
+    if (w > 5) draw(x - w, y + w, w / 2);
+    if (w > 5) draw(x + w, y + w, w / 2);
+}
+
+function line(x, y, x1, y1) {
+    c.beginPath();
+    c.moveTo(x, y);
+    c.lineTo(x1, y1);
+    c.stroke();
+}
+
+function sizeCanvas() {
+    height = window.innerHeight;
+    width = window.innerWidth;
+    canvas.height = height;
+    canvas.width = width;
+}
+
+window.onresize = function() {
+    sizeCanvas();
+    draw(width / 2, 100, 100);
+};
